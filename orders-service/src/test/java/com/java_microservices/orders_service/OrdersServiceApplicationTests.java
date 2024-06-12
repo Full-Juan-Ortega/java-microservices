ca3574f45627f360f766922f8dff4900adb72d1e
Juan Ortega <juan.ortega.it@gmail.com>
2024-06-12 12:01:36 -0300

    fixed standard configs database an microservices conections

---
+++ orders-service/src/test/java/com/java_microservices/orders_service/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_microservices.orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
